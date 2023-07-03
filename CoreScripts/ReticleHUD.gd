54678f3882e6c5797528e7ab726e90da08331666
Stepan Shabalin <stomperhomp@gmail.com>
2023-07-03 18:29:05 +0400

    Add reticles, try animating them

--- CoreScripts/ReticleHUD.gd
+++ CoreScripts/ReticleHUD.gd
@@ -1,11 +1,12 @@
 extends Control
 
 export var mouse_sensitivity = 2.0
+export var ray_max = 100
 onready var reticle = $Reticle
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -13,8 +14,22 @@ func _input(event):
 		reticle.rect_position += movement
 		reticle.rect_position.x = max(0, min(rect_size.x, reticle.rect_position.x))
 		reticle.rect_position.y = max(0, min(rect_size.y, reticle.rect_position.y))
-	elif event is InputEventMouseButton and event.button_index == 0:
+	elif event is InputEventMouseButton and event.button_index == 1 and event.pressed:
 		activate()
 
+func _process(delta):
+	if cast_ray():
+		pass
+	else:
+		pass
+
 func activate():
-	pass
+	print(cast_ray())
+
+func cast_ray():
+	var coords = reticle.rect_global_position
+	var camera = get_viewport().get_camera()
+	var source = camera.project_ray_origin(coords)
+	var direction = camera.project_ray_normal(coords)
+	var dss = get_parent().get_world().direct_space_state
+	return dss.intersect_ray(source, direction * ray_max)
