40ce94021d7c0edc36c917d31be1cfce80e4194a
Stepan Shabalin <stomperhomp@gmail.com>
2023-07-04 15:42:08 +0400

    Add basic main menu

---
+++ Scenes/StartingMenu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+onready var animation = $AnimationPlayer
+# 0: waiting for key press
+var state = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	animation.current_animation = "press_key"
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if state == 0:
+		for i in range(512):
+			if Input.is_key_pressed(i):
+				state = 1
+				animation.current_animation = "menu"
+				return
